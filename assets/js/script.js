ecc0134b71383ac8a37d6b60de3a173a4c4b5e4d
vaishnavi <vaishnavip@prdxn.com>
2020-05-19 18:11:23 +0530

    function to block forward history and don selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 /* Author: 
 
 */
+'use strict'
+window.onload = (function(){
+    window.history.forward();
+    function blockBack() {
+        window.history.forward();
+    }
+
+    //selecting DOM
+    var main=this.document.querySelector("main");
+    
+    //index-page/login-page functionality starts here
+    var loginPage = document.getElementById("login-page");
+    var login = document.querySelector("login-btn");
+    var modal = document.getElementById("form-modal");
+    var closeMod = document.getElementById("close");
+
+    
+
+})
 
 
 
